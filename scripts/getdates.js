fdd696b7886639b47e15d31c6a02c7b5bc0458fa
Quinlzbeth <shonibare109@gmail.com@byupathway.edu>
2026-06-19 20:34:02 +0100

    commit

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Set current year
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = `©${year} 💐Elizabeth Shonibare💐 Nigeria`;
+
+// Set last modified date
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
